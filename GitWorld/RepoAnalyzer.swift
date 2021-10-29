e84df1c589854430286c5ac693fcb433711d06fc
Aleksandr Motarykin <aleksandr.motarykin@revelsystems.com>
2021-10-29 12:56:12 +0300

    Added RemoAnalyzer

--- GitWorld/RepoAnalyzer.swift
+++ GitWorld/RepoAnalyzer.swift
@@ -10,10 +10,15 @@ import Foundation
 class RepoAnalyzer {
     private let fileManager: FileManager
     private let localURL: URL
+    private let filesURL: [URL]
     
     init(localURL: URL, fileManager: FileManager = FileManager.default) {
         self.fileManager = fileManager
         self.localURL = localURL
+        self.filesURL = RepoAnalyzer.getFilesPathsToAnalyze(fileManager: fileManager, localURL: localURL)
     }
     
+    private static func getFilesPathsToAnalyze(fileManager: FileManager, localURL: URL) -> [URL] {
+        filesURL = fileManager.contentsOfDirectory(at: localURL, includingPropertiesForKeys: nil, options: [])
+    }
 }
